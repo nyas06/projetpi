0418b7a74fa59b86d3b1cfc9fc37c90302fd2460
nyas06 <nyas06@gmail.com>
2017-04-02 00:47:17 +0200

    modif sensor.cpp

--- sensor/sensor.cpp
+++ sensor/sensor.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <wiringPi.h>
 #include <unistd.h>
-#include <ctime>
 
 #define 	TRIG		4  //port GPIO relais HF1
 #define		ECHO		5  //port GPIO relais HF2
@@ -22,24 +21,31 @@ using namespace std;
 	if(wiringPiSetup() == -1) {cout << "--> Erreur WiringPI " << endl;return -1;}
 	// activation des ports GPIO
 	pinMode(TRIG, OUTPUT);
-	pinMode(ECHO, OUTPUT);
+	pinMode(ECHO, INTPUT);
 	
 	// declaration des variables repetition, selection selcal civil ou militaire,poste HF et indicatif
-	string chaineRepetition,chaineTypeSelcal, chainePosteHf, chaineIndicatif;
-	int nombreDeRepetition = 0,typeDeSecal=0, selectHF = 0;
+	unsigned int pulseStart,pulseStop,pulseInterval,distance;
 
 	digitalWrite(TRIG,LOW);
 
 
     digitalWrite(TRIG,HIGH);
-
+    usleep(10);
+    digitalWrite(TRIG,LOW);
 	// initialisation de la librairie audio
 	usleep(10000);
-	
-		//retour à l'état repos des relais
-		digitalWrite(EM1,HIGH);
-		digitalWrite(EM2,HIGH);
-		digitalWrite(EM3,HIGH);
+
+	while(digitalRead(ECHO)==0){
+        pulseStart=micros();
+    }
+
+    while(digitalRead(ECHO)==1){
+        pulseStop=micros();
+    }
+    
+    pulseInterval=pulseStart-pulseStop;
+    distance = pulseInterval*0.170;
+	cout << distance << endl;
 	
 	
   }  // fin fonction main
